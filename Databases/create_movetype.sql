a0960a16acbc3742b363c874bddd98b06890870e
Nissinen <frazento@hotmail.com>
2016-04-01 13:13:42 +0300

    tietokannan harkkaan

--- Databases/create_movetype.sql
+++
@@ -1,6 +0,0 @@
-create table dbo.liiketyyppi(
-
-  Avain int identity(1,1) not null,
-  
-  constraint PK_liiketyyppi_Avain primary key (avain)
-)
--- Databases/create_trainingtype.sql
+++
@@ -1,8 +0,0 @@
-create table dbo.treenityyppi(
-
-Avain int identity(1,1) not null,
-Tunniste nvarchar(20) not null,
-  
- constraint PK_treenityyppi_Avain primary key (avain),
- constraint UQ_treenityyppi_Tunniste unique (tunniste)
-)
